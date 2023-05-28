99a6e91df02b0dbd6f99fbf30295aca830c15ce8
c0natus <hks8946@gmail.com>
2023-05-28 17:17:49 +0900

    visualization

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ python=3.10
 pytorch=1.12.1
 torhvision=0.13.1
 cuda=11.6
+python-box=7.0.1
 ```
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh  
 
-#SBATCH -J gevit_40
-#SBATCH -o ./output/gevit_40.out
+#SBATCH -J vit_24
+#SBATCH -o ./output/vit_24.out
 #SBATCH -t 3-00:00:00
 
-#SBATCH --nodelist=n42
-#SBATCH -p A6000
-#SBATCH --gres=gpu:4
+#SBATCH --nodelist=n37
+#SBATCH -p A5000
+#SBATCH --gres=gpu:2
 
 #SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
@@ -17,7 +17,7 @@ echo "Start"
 
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate deepvit
-torchrun --nproc_per_node=4 train.py --world-size 4 --workers 10 --batch-size 32 --sync-bn --layer-num 40 --model-type gevit --data-path ./dataset/CIFAR100 --dataset-name cifar
+torchrun --nproc_per_node=2 train.py --world-size 2 --workers 10 --batch-size 64 --sync-bn --layer-num 24 --model-type vit --data-path ./dataset/CIFAR100 --dataset-name cifar
 conda deactivate
 
 echo "Done!"
