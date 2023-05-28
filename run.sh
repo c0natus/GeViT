559113a2ddfe1d40eb6459826f38ba35831b10b6
c0natus <hks8946@gmail.com>
2023-05-28 11:08:26 +0900

    training code

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh  
 
-#SBATCH -J deepvit_32
-#SBATCH -o ./output/deepvit_32.out
-#SBATCH -t 3-00:00:00 
+#SBATCH -J gevit_40
+#SBATCH -o ./output/gevit_40.out
+#SBATCH -t 3-00:00:00
 
-#SBATCH --nodelist=n46
+#SBATCH --nodelist=n42
 #SBATCH -p A6000
 #SBATCH --gres=gpu:4
 
@@ -17,7 +17,7 @@ echo "Start"
 
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate deepvit
-torchrun --nproc_per_node=4 train.py --world-size 4 --workers 10 --batch-size 32 --sync-bn --layer-num 32 --model-type deepvit --data-path ./dataset/CIFAR100 --dataset-name cifar
+torchrun --nproc_per_node=4 train.py --world-size 4 --workers 10 --batch-size 32 --sync-bn --layer-num 40 --model-type gevit --data-path ./dataset/CIFAR100 --dataset-name cifar
 conda deactivate
 
 echo "Done!"
